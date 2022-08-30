393d34baeadf13504958d661eeaf9192a00be82e
Thomas Antonini <tantonini@baylibre.com>
2022-08-30 12:39:54 +0200

    Add nunchuk device initialization
    Signed-off-by: Thomas Antonini <tantonini@baylibre.com>

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
@@ -6,7 +7,24 @@
 static int nunchuk_i2c_probe(struct i2c_client *client,
 		const struct i2c_device_id * device_id)
 {
-	pr_info("Probe\n");
+	int ret;
+	unsigned char buffer[2];
+
+	buffer[0] = 0xf0;
+	buffer[1] = 0x55;
+
+	ret = i2c_master_send(client, buffer, 2);
+	if (ret < 0)
+		return ret;
+
+	udelay(1000);
+
+	buffer[0] = 0xfb;
+	buffer[1] = 0x00;
+
+	ret = i2c_master_send(client, buffer, 2);
+	if (ret < 0)
+		return ret;
 
 	return 0;
 }
